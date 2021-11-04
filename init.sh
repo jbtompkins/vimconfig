813ddce4225394a4a4b8e468d3c056d739af9941
James Tompkins <james.b.tompkins@gmail.com>
2021-11-03 22:24:53 -0700

    Added init script.

---
+++ init.sh
@@ -0,0 +1,9 @@
+cd ~
+# Check if .vimrc exists. If so, rename it.
+if test -f .vimrc; then
+    echo "Existing .vimrc found; renaming to vimrc_old"
+    mv .vimrc vimrc_old
+fi
+# Write new vimrc to use vimrc in .vim directory
+echo "Writing new .vimrc"
+echo "runtime vimrc" > .vimrc
